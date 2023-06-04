f2bfdc439973814ad524ed131a4a3cd4bbaa15d7
OleksVov <112511040+OleksVov@users.noreply.github.com>
2023-06-04 23:08:25 +0000

    Deploying to gh-pages from @ OleksVov/tweet-card@9402f9a66191be60713f7fb6314b67d68329e271 🚀

---
+++ static/js/75.6f89addc.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[75],{75:function(e,t,n){n.r(t),n.d(t,{default:function(){return c}});var i=n(184),r={container:{minHeight:"calc(100vh - 50px)",display:"flex",alignItems:"center",justifyContent:"center"},title:{fontWeight:600,fontSize:48,textAlign:"center"}};function c(){return(0,i.jsx)("div",{style:r.container,children:(0,i.jsx)("h1",{style:r.title,children:"Welcome to the Twitter"})})}}}]);
+//# sourceMappingURL=75.6f89addc.chunk.js.map
